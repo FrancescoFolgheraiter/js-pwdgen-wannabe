832ead8e5d3cfcbff9af59f7e11bdd2159b5799a
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2023-11-30 14:38:24 +0100

    update with comment

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,12 @@ const cognome = prompt("Insserisci il tuo cognome");
 const colore = prompt("Inserisci il tuo colore preferito");
 let numero=Math.floor(Math.random() * 101);
 let password=`La password generata è la seguente:${nome}${cognome}${colore}${numero}`
+console.log(password)
 //altra possibilità era di utilizzare i +
 document.writeln(password);
+
+/*
+altra possibilità invece di utilizzare  document.writelg()
+è l'utilizzo della selezione per id
+document.getElementById("password").innerHTML = (password);
+*/
